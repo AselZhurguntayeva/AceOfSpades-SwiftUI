8a078ef71d6c0bb0d4bae8a88e6deac55f3f8443
assollzh@gmail.com <assollzh@gmail.comm>
2022-07-05 15:28:37 -0700

    Initial Commit

---
+++ AceOfSpades/AceOfSpadesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AceOfSpadesApp.swift
+//  AceOfSpades
+//
+//  Created by Viktoriya Kudryashova on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AceOfSpadesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
